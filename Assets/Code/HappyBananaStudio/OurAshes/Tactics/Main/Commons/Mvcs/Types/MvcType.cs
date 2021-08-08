b1e804a2f37dd59181a9624a148a1fe7cad07996
Matthew Gintjee <mgintjee@gmail.com>
2021-08-08 11:31:12 -0700

    HBS-1:
    Adding more utility classes
    Adding common enums

---
+++ Assets/Code/HappyBananaStudio/OurAshes/Tactics/Main/Commons/Mvcs/Types/MvcType.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Code.HappyBananaStudio.OurAshes.Tactics.Main.Commons.Mvcs.Types
+{
+    /// <summary>
+    /// Todo
+    /// </summary>
+    public enum MvcType
+    {
+        None,
+        Sortie,
+        Central,
+        Home,
+        World
+    }
+}
---
+++ Assets/Code/HappyBananaStudio/OurAshes/Tactics/Main/Commons/Sprites/IDs/SpriteID.cs
@@ -0,0 +1,19 @@
+﻿namespace Assets.Code.HappyBananaStudio.OurAshes.Tactics.Main.Commons.Sprites.IDs
+{
+    /// <summary>
+    /// Todo
+    /// </summary>
+    public enum SpriteID
+    {
+        None,
+        Circle,
+        RoundedSquare,
+        Square,
+        TriangleUp,
+        TriangleDown,
+        DiamondVertical,
+        DiamondHorizontal,
+        CapsuleVertical,
+        CapsuleHorizontal,
+    }
+}
